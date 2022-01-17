484eaf3bd8a43f24fc99d8ef0f95801592ce7a88
snook9 <jonathan.cassaing@free.fr>
2022-01-17 14:12:31 +0100

    Moving hello_world.yml to swagger folder

--- web_service/__init__.py
+++ web_service/__init__.py
@@ -54,5 +54,24 @@ def create_app(test_config=None):
 
     return app
 
-swagger_template = dict(info = {    'title': LazyString(lambda: 'My first Swagger UI document'),    'version': LazyString(lambda: '0.1'),    'description': LazyString(lambda: 'This document depicts a      sample Swagger UI document and implements Hello World functionality after executing GET.'),    },    host = LazyString(lambda: request.host))
-swagger_config = {    "headers": [],    "specs": [        {            "endpoint": 'hello_world',            "route": '/hello_world.json',            "rule_filter": lambda rule: True,            "model_filter": lambda tag: True,        }    ],    "static_url_path": "/flasgger_static",    "swagger_ui": True,    "specs_route": "/apidocs/"}
+swagger_template = dict(
+    info = {
+        'title': LazyString(lambda: 'My first Swagger UI document'),
+        'version': LazyString(lambda: '0.1'),
+        'description': LazyString(lambda: 'This document depicts a sample Swagger UI document and implements Hello World functionality after executing GET.'),
+    },
+    host = LazyString(lambda: request.host)
+)
+swagger_config = {
+    "headers": [],
+    "specs": [
+        {
+        "endpoint": 'hello_world',
+        "route": '/hello_world.json',
+        "rule_filter": lambda rule: True,
+        "model_filter": lambda tag: True,
+    }],
+    "static_url_path": "/flasgger_static",
+    "swagger_ui": True,
+    "specs_route": "/apidocs/"
+}
