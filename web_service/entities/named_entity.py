766e3b0b23a8a0ed19be05b5d56d6a9637b8b976
snook9 <jonathan.cassaing@free.fr>
2022-02-11 17:31:12 +0100

    Switching casse for enum

--- web_service/entities/named_entity.py
+++ web_service/entities/named_entity.py
@@ -12,32 +12,32 @@ class NamedEntityScoreEnum(Enum):
     # The score is based on the number of models that detect the entity.
     # The used models are AWS Comprehend, NLTK and Spacy
     # The score is high if all models detected the entity
-    HIGH = "High"
+    HIGH = "HIGH"
     # The score is medium if 2 models detected the entity
-    MEDIUM = "Medium"
+    MEDIUM = "MEDIUM"
     # The score is low if only 1 model detected the entity
-    LOW = "Low"
+    LOW = "LOW"
 
 class NamedEntityTypeEnum(Enum):
     """Enum for the type of a named entity"""
     # A branded product
-    PRODUCT = "Product"
+    PRODUCT = "PRODUCT"
     # A full date (for example, 11/25/2017), day (Tuesday), month (May), or time (8:30 a.m.)
-    DATE = "Date"
+    DATE = "DATE"
     # An event, such as a festival, concert, election, etc.
-    EVENT = "Event"
+    EVENT = "EVENT"
     # A specific location, such as a country, city, lake, building, etc.
-    LOCATION = "Location"
+    LOCATION = "LOCATION"
     # Large organizations, such as a government, company, religion, sports team, etc.
-    ORGANIZATION = "Organizartion"
+    ORGANIZATION = "ORGANIZATION"
     # Individuals, groups of people, nicknames, fictional characters
-    PERSON = "Person"
+    PERSON = "PERSON"
     # A quantified amount, such as currency, percentages, numbers, bytes, etc.
-    QUANTITY = "Quantity"
+    QUANTITY = "QUANTITY"
     # An official name given to any creation or creative work, such as movies, books, songs, etc.
-    TITLE = "Title"
+    TITLE = "TITLE"
     # Entities that don't fit into any of the other entity categories
-    OTHER = "Other"
+    OTHER = "OTHER"
 
 class NamedEntity:
     """Named entity class"""
